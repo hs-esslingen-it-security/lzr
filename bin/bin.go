f8d4fae110280122c57ebf3bb3f9b4f3a5f0f050
nlohmill <nils.lohmiller@hs-esslingen.de>
2024-11-21 16:19:36 +0100

    remove log line

--- bin/bin.go
+++ bin/bin.go
@@ -104,7 +104,7 @@ func LZRMain() {
 			}
 
 			for input := range incoming {
-				fmt.Fprintln(os.Stderr, "for incomings ipMeta count:", ipMeta.Count())
+				//fmt.Fprintln(os.Stderr, "for incomings ipMeta count:", ipMeta.Count())
 				if lzr.ReadZMap() {
 					toACK := true
 					toPUSH := false
